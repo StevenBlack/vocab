0f88e0dab1bd3f9c48b6c468bb80a9694a9fe2b3
Steven Black <steveb@stevenblack.com>
2020-08-27 21:20:58 -0400

    Accept command line argiments and implement -i for index only.

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,15 @@ use std::{
     io::{prelude::*, BufReader},
     path::Path,
 };
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "example", about = "An example of StructOpt usage.")]
+struct Opt {
+    /// Just index
+    #[structopt(short, long)]
+    index: bool,
+}
 
 fn lines_from_file(filename: impl AsRef<Path>) -> Vec<String> {
     let file = File::open(filename).expect("no such file");
@@ -29,6 +38,7 @@ impl Display for Entry {
 // ---
 
 fn main() {
+    let opt = Opt::from_args();
     let lines = lines_from_file("./million-dollar-words.txt");
     let entries = lines.split(|l| l == "");
     let mut dictionary: Vec<Entry> = Vec::new();
@@ -45,10 +55,16 @@ fn main() {
     }
 
     // sort the dictionary
-    dictionary.sort_by(|t1, t2| t1.term.cmp(&t2.term));
+    dictionary.sort_by(|t1, t2| t1.term.to_lowercase().cmp(&t2.term.to_lowercase()));
 
     // print the dictionary
-    for word in dictionary.iter() {
-        println!("{}", word);
+    if opt.index {
+        for word in dictionary.iter() {
+            println!("{}", word.term);
+        }
+    } else {
+        for word in dictionary.iter() {
+            println!("{}", word);
+        }
     }
 }
