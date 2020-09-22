7f2d87cfbfef72fd6f2363e0af1713f2ab858a35
Steven Black <steveb@stevenblack.com>
2020-09-22 17:30:53 -0400

    Now loading the toc vec with characters and targets.

--- src/main.rs
+++ src/main.rs
@@ -38,6 +38,11 @@ impl Display for Entry {
     }
 }
 
+#[derive(Debug)]
+struct TOCEntry {
+    pub term: String,
+    pub letter: String,
+}
 // ---
 
 fn main() {
@@ -45,7 +50,7 @@ fn main() {
     let lines = lines_from_file("./million-dollar-words.txt");
     let entries = lines.split(|l| l == "");
     let mut dictionary: Vec<Entry> = Vec::new();
-    let mut toc: Vec<String> = Vec::new();
+    let mut toc: Vec<TOCEntry> = Vec::new();
 
     for entry in entries {
         let word = &entry[0];
@@ -61,6 +66,21 @@ fn main() {
     // sort the dictionary
     dictionary.sort_by(|t1, t2| t1.term.to_lowercase().cmp(&t2.term.to_lowercase()));
 
+    if opt.toc {
+        let mut letter = "".to_string();
+        for entry in dictionary.iter() {
+            let l = entry.term.chars().next().unwrap().to_string().to_lowercase();
+            if letter != l {
+                let newletter = l.to_string();
+                letter = newletter;
+                let term = TOCEntry {
+                    term: entry.term.to_string(),
+                    letter: l.to_string(),
+                };
+                toc.push(term);
+            }
+        }
+    }
     // print the dictionary
     if opt.index {
         for word in dictionary.iter() {
@@ -70,7 +90,7 @@ fn main() {
         // print the toc
         if opt.toc {
             if toc.len() > 0 {
-              println!("## Contents\n\n{:?}\n\n", toc);
+              println!("## Contents\n\n{:?} ({} items)\n\n", toc, toc.len());
             } else {
               println!("## Contents\n\n");
             }
